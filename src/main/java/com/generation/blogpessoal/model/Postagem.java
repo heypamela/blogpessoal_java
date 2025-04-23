198315102a21796ff0741e14ebce640f1b0f7be3
Pamela <pamsilva.trabalho@hotmail.com>
2025-04-23 12:43:07 -0300

    Added the security part to the project

--- src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -37,27 +37,38 @@ public class Postagem {
     @JsonIgnoreProperties("postagem")
     private Tema tema;
 
+    @ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Usuario usuario;
+
     public Long getId() {
         return id;
     }
+    
     public void setId(Long id) {
         this.id = id;
     }
+    
     public String getTitulo() {
         return titulo;
     }
+    
     public void setTitulo(String titulo) {
         this.titulo = titulo;
     }
+    
     public String getTexto() {
         return texto;
     }
+    
     public void setTexto(String texto) {
         this.texto = texto;
     }
+    
     public LocalDate getData() {
         return data;
     }
+    
     public void setData(LocalDate data) {
         this.data = data;
     }
@@ -65,7 +76,16 @@ public class Postagem {
     public Tema getTema() {
         return tema;
     }
+
     public void setTema(Tema tema) {
         this.tema = tema;
     }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }    
 }
